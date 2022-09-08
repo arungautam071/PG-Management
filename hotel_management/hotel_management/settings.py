7cb672d801e676db5992f6b3b558ddaea989764f
aarun gautam <arungautam071@gmail.com>
2022-09-08 21:22:47 +0530

    change host

--- hotel_management/hotel_management/settings.py
+++ hotel_management/hotel_management/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pgmanagementapp.herokuapp.com']
+ALLOWED_HOSTS = ['shivshivaresidency.herokuapp.com']
 
 
 # Application definition
